dd91c870c33bb63de63d992ec24c61cfe6058823
Tona23 <tona23.tona14@gmail.com>
2021-07-08 22:08:31 -0500

    T0

--- app/src/main/java/com/example/app_control/Principal.java
+++ app/src/main/java/com/example/app_control/Principal.java
@@ -3,18 +3,22 @@ package com.example.app_control;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,6 +27,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class Principal extends AppCompatActivity implements OnMapReadyCallback ,GoogleMap.OnMarkerClickListener,GoogleMap.OnMarkerDragListener, GoogleMap.OnInfoWindowClickListener  {
 
@@ -52,6 +62,9 @@ public class Principal extends AppCompatActivity implements OnMapReadyCallback ,
         tv_tiempotxt = (TextView)findViewById(R.id.tv_c_tiempotxt);
         tv_conductortxt = (TextView)findViewById(R.id.tv_c_conductortxt);
         tv_disponibilidadtxt = (TextView)findViewById(R.id.tv_c_disponibilidadtxt);
+        tv_rutas = (TextView)findViewById(R.id.tv_c_rutas);
+        tv_ficha = (TextView)findViewById(R.id.tv_c_ficha);
+
         img_conductor = (ImageView)findViewById(R.id.img_c_conductor);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -74,7 +87,7 @@ public class Principal extends AppCompatActivity implements OnMapReadyCallback ,
             return;
         }
 
-        if(mMap.isMyLocationEnabled()==false){
+        if(!mMap.isMyLocationEnabled()){
             LatLng CDMX= new LatLng(19.3168, -99.08671 );
             mMap.addMarker(new MarkerOptions().position(CDMX).title("Marcador en la CDMX"));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(CDMX));
@@ -85,41 +98,63 @@ public class Principal extends AppCompatActivity implements OnMapReadyCallback ,
         LocationListener locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                if(mMap.isMyLocationEnabled()==true&&UbiAct==false){
+                if(!mMap.isMyLocationEnabled()&&!UbiAct){
                     mMap.clear();
                     LatLng TiempoReal= new LatLng(location.getLatitude(), location.getLongitude() );
-                    mMap.addMarker(new MarkerOptions().position(TiempoReal).title("Marcador de tu Ubicacion"));
+                    markerPerso = googleMap.addMarker(new MarkerOptions().position(TiempoReal).draggable(true).title("Tu ubicacion"));
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(TiempoReal));
+                    info = Principal.this.setLocation(location);
                     UbiAct=true;
                 }
             }
 
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
+                switch (status) {
+                    case LocationProvider.AVAILABLE:
+                        Log.d("debug", "LocationProvider.AVAILABLE");
+                        break;
+                    case LocationProvider.OUT_OF_SERVICE:
+                        Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                        break;
+                    case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                        Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                        break;
+                }
             }
 
             @Override
             public void onProviderEnabled(String provider) {
+                Toast.makeText(Principal.this,"GPS Activado", LENGTH_SHORT).show();
             }
 
             @Override
             public void onProviderDisabled(String provider) {
+                Toast.makeText(Principal.this,"GPS Desactivado", LENGTH_SHORT).show();
             }
         } ;
 
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,locationListener);
 
+        googleMap.setOnMarkerClickListener(this);
+        googleMap.setOnMarkerDragListener(this);
+        googleMap.setOnInfoWindowClickListener(this);
         mMap.setMyLocationEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
     }
 
     @Override
     public void onInfoWindowClick(Marker marker) {
-
+        if(marker.equals(markerPerso)){
+            Fragmento.newInstance(marker.getTitle(),info).show(getSupportFragmentManager(), null);
+        }
     }
 
     @Override
     public boolean onMarkerClick(Marker marker) {
+        if(marker.equals(markerPerso)){
+            Toast.makeText(this,"Manten apretado para mover ubicacion",Toast.LENGTH_LONG).show();
+        }
         return false;
     }
 
@@ -130,11 +165,57 @@ public class Principal extends AppCompatActivity implements OnMapReadyCallback ,
 
     @Override
     public void onMarkerDrag(Marker marker) {
-
+        if(marker.equals(markerPerso)){
+            Toast.makeText(this,"Posicion", LENGTH_SHORT).show();
+            String newTitle = String.format(Locale.getDefault(), getString(R.string.marker_detail_lating), marker.getPosition().latitude, marker.getPosition().longitude);
+            setTitle(newTitle);
+        }
     }
 
     @Override
     public void onMarkerDragEnd(Marker marker) {
+        if(marker.equals(markerPerso)){
+            setTitle(R.string.app_name);
+            if(markerPerso.getPosition().longitude !=0.0 && markerPerso.getPosition().latitude !=0.0) {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
 
+                List<Address> list;
+                try {
+                    list = geocoder.getFromLocation(
+                            markerPerso.getPosition().latitude, markerPerso.getPosition().longitude, 1);
+                    if (!list.isEmpty()) {
+                        Address DirCalle = list.get(0);
+
+                        direccion = DirCalle.getAddressLine(0);
+                        info = direccion;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            markerPerso.setTitle("Tu ubicacion");
+        }
+    }
+
+    public String setLocation(Location loc) {
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (!mMap.isMyLocationEnabled()) {
+            try {
+
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    Address DirCalle = list.get(0);
+
+                    direccion = DirCalle.getAddressLine(0);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return direccion;
     }
 }
+
